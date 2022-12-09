0df4d0709b49dc84d4386e1b3d1bf47460ae64aa
张欧文 <1534661514@qq.com>
2022-12-09 21:38:01 +0800

    update layers

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ const preText = document.querySelector(".pre-text");
 const formEl = document.querySelector(".form-input");
 const updateButton = document.querySelector(".update-savings");
 
+const initDate = new Date("December 1, 2022");
 const initSavings = 200000;
 
 let curTotalSavings = 200000;
 let yearlyRate = 0;
 let tenYearPrediction = 0;
-let passingDays = 10;
+let passingDays = (new Date() - initDate) / (1000 * 3600 * 24);
+console.log(passingDays);
 
 let savingsData = {
-  date: [new Date("December 31, 2020"), new Date("December 31, 2021")],
-  curTotalSavingsList: [200000, 220000],
-  yearlyRateList: [0.4, 0.5],
-  tenYearPrediction: [300000, 500000],
+  date: [initDate],
+  curTotalSavingsList: [initSavings],
+  yearlyRateList: [0],
+  tenYearPrediction: [0],
 };
 
 const init = function () {
@@ -28,15 +30,18 @@ init();
 const calcYearlyRate = function (
   curTotalSavings,
   initSavings = 200000,
-  passingDays = 10
+  passingDays = new Date() - initDate
 ) {
   return (
-    Math.pow(Math.pow(curTotalSavings / initSavings, 1 / passingDays), 365) - 1
+    // Math.pow(Math.pow(curTotalSavings / initSavings, 1 / passingDays), 365) - 1
+    (((curTotalSavings - initSavings) / passingDays) * 365) / initSavings
   );
 };
 
-const calcTenYearsPrediction = function (curTotalSavings, yearlyRate) {
-  return curTotalSavings * Math.pow(1 + yearlyRate, 10);
+const calcTenYearsPrediction = function (curTotalSavings) {
+  return (
+    curTotalSavings + ((curTotalSavings - initSavings) / passingDays) * 365 * 10
+  );
 };
 
 // console.log(calcTenYearsPrediction(10, 0.1));
@@ -72,15 +77,15 @@ updateButton.addEventListener("click", function () {
   // console.log(passingDays);
   yearlyRate = calcYearlyRate(curTotalSavings, initSavings, passingDays);
   //   console.log(yearlyRate);
-  tenYearPrediction = calcTenYearsPrediction(curTotalSavings, yearlyRate);
-  updateText(curTotalSavings, yearlyRate, tenYearPrediction);
+  tenYearPrediction = calcTenYearsPrediction(curTotalSavings);
   updateData(curTotalSavings, yearlyRate, tenYearPrediction);
+  updateText(curTotalSavings, yearlyRate, tenYearPrediction);
   updateFigure();
 });
 
 // data for the line chart
 const updateFigure = function () {
-  let data = [
+  let dataTotal = [
     {
       x: savingsData.date,
       y: savingsData.curTotalSavingsList,
@@ -90,20 +95,75 @@ const updateFigure = function () {
       },
     },
   ];
-  let layout = {
-    title: "Line Chart",
+
+  let dataRate = [
+    {
+      x: savingsData.date,
+      y: savingsData.yearlyRateList,
+      type: "scatter",
+      marker: {
+        color: "red",
+      },
+    },
+  ];
+
+  let layoutTotal = {
+    font: {
+      family: "Noto Sans SC, sans-serif",
+      size: 18,
+      weight: 700,
+      color: "#fff",
+    },
+    title: {
+      text: "存款总额",
+      x: 0.5,
+      y: 0.0085,
+    },
     xaxis: {
-      title: "日期",
+      // title: "",
       showgrid: false,
       zeroline: false,
     },
     yaxis: {
-      title: "存款余额",
+      // title: "存款总额",
       showline: false,
     },
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: "rgba(0,0,0,0)",
   };
-  Plotly.newPlot("fig-total", data, layout);
-  Plotly.newPlot("fig-rate", data, layout);
-};
 
+  let layoutRate = {
+    font: {
+      family: "Noto Sans SC, sans-serif",
+      size: 18,
+      weight: "bold",
+      color: "#fff",
+    },
+    title: {
+      text: "平均年化增长率",
+      x: 0.5,
+      y: 0.0085,
+    },
+    xaxis: {
+      // title: "",
+      showgrid: false,
+      zeroline: false,
+    },
+    yaxis: {
+      // title: "平均年化增长率",
+      showline: false,
+      tickformat: "%",
+    },
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: "rgba(0,0,0,0)",
+  };
+  Plotly.newPlot("fig-total", dataTotal, layoutTotal);
+  Plotly.newPlot("fig-rate", dataRate, layoutRate);
+};
+// console.log(savingsData.curTotalSavingsList.slice(-1)[0]);
+updateText(
+  savingsData.curTotalSavingsList.slice(-1)[0],
+  savingsData.yearlyRateList.slice(-1)[0],
+  savingsData.tenYearPrediction.slice(-1)[0]
+);
 updateFigure();
